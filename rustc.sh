165a568b9f0627d98eb313ce7740219206742924
bjorn3 <bjorn3@users.noreply.github.com>
2020-09-08 11:42:17 +0200

    [WIP]

--- rustc.sh
+++ rustc.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-wasmtime run /home/bjorn/Documenten/rust2/target/wasm32-wasi/release/rustc_binary.wasm --dir / -- $@
+if echo "$@" | grep -q -- "--target"; then
+wasmtime run /home/bjorn/Documenten/rust2/target/wasm32-wasi/release/rustc_binary.wasm --dir / --dir /tmp --mapdir sysroot_src::/home/bjorn/Documenten/cg_clif/build_sysroot/sysroot_src --mapdir alloc_system::/home/bjorn/Documenten/cg_clif/build_sysroot/alloc_system -- --sysroot /home/bjorn/Documenten/cg_clif/build_sysroot/sysroot $@
+else
+rustc $@
+fi
