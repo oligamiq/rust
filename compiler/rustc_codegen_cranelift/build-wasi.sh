c570c761680053310ef58b0c75bfc003c29f8f4e
bjorn3 <bjorn3@users.noreply.github.com>
2021-04-04 19:13:18 +0200

    [WIP]

--- compiler/rustc_codegen_cranelift/build-wasi.sh
+++ compiler/rustc_codegen_cranelift/build-wasi.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 RUSTC_INSTALL_BINDIR=bin CFG_COMPILER_HOST_TRIPLE=wasm32-wasi CFG_RELEASE=1.53.0-nightly CFG_RELEASE_CHANNEL=nightly cargo +nightly build --release --target wasm32-wasi
-RUSTC=rustc-wrapper.sh cargo check --manifest-path=../../library/test/Cargo.toml --target-dir wasi-target --release --target x86_64-unknown-linux-gnu --locked
+RUSTC=$(pwd)/rustc-wrapper.sh RUSTFLAGS="--sysroot /" CARGO_TARGET_DIR=$(pwd)/wasi-target cargo build --manifest-path=../../library/std/Cargo.toml --release --target x86_64-unknown-linux-gnu --locked
--- compiler/rustc_codegen_cranelift/rustc-wrapper.sh
+++ compiler/rustc_codegen_cranelift/rustc-wrapper.sh
@@ -5,7 +5,7 @@ dir=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
 
 for arg in "$@"; do
     if [[ "$arg" = --target* ]]; then
-        exec -- wasmtime run "$dir/target/wasm32-wasi/release/cg_clif.wasm" "$@"
+	    exec -- wasmtime run "$dir/target/wasm32-wasi/release/cg_clif.wasm" --dir /tmp --dir $(pwd) --dir . --dir $CARGO_TARGET_DIR --env STD_ENV_ARCH=${STD_ENV_ARCH:-a} -- "$@"
     fi
 done
 
