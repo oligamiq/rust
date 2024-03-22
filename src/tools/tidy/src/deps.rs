0ba8121d79a46a9906b36455f52601362d69527d
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-03-22 20:11:32 +0000

    Remove support for decompressing dylib metadata
    We haven't been compressing dylib metadata for a while now. Removing
    decompression support will regress error messages about an incompatible
    rustc version being used, but dylibs are pretty rare anyway.

--- src/tools/tidy/src/deps.rs
+++ src/tools/tidy/src/deps.rs
@@ -91,7 +91,6 @@ const EXCEPTIONS: ExceptionList = &[
     ("rustc_apfloat", "Apache-2.0 WITH LLVM-exception"),     // rustc (license is the same as LLVM uses)
     ("ryu", "Apache-2.0 OR BSL-1.0"), // BSL is not acceptble, but we use it under Apache-2.0                       // cargo/... (because of serde)
     ("self_cell", "Apache-2.0"),                             // rustc (fluent translations)
-    ("snap", "BSD-3-Clause"),                                // rustc
     ("wasm-encoder", "Apache-2.0 WITH LLVM-exception"),      // rustc
     ("wasmparser", "Apache-2.0 WITH LLVM-exception"),        // rustc
     // tidy-alphabetical-end
@@ -335,7 +334,6 @@ const PERMITTED_RUSTC_DEPENDENCIES: &[&str] = &[
     "sharded-slab",
     "shlex",
     "smallvec",
-    "snap",
     "stable_deref_trait",
     "stacker",
     "static_assertions",
