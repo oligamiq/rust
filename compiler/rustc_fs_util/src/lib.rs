d77d78b18c066bec58ceda496a964cd91aed08a2
oligamiq <nziq53@gmail.com>
2024-09-01 16:00:58 +0900

    Compile rustc for wasm15 with llvm

--- compiler/rustc_fs_util/src/lib.rs
+++ compiler/rustc_fs_util/src/lib.rs
@@ -84,7 +84,7 @@ pub fn path_to_c_string(p: &Path) -> CString {
     let p: &OsStr = p.as_ref();
     CString::new(p.as_bytes()).unwrap()
 }
-#[cfg(windows)]
+#[cfg(any(windows, target_os = "wasi"))]
 pub fn path_to_c_string(p: &Path) -> CString {
     CString::new(p.to_str().unwrap()).unwrap()
 }
